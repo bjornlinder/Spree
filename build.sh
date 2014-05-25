db3bc8fe348b0ff055fc537c7f618015a110de41
Bjorn Linder <bjornishungry@hotmail.com>
2014-05-25 14:49:37 -0400

    removed sample & frontend folders

--- build.sh
+++ build.sh
@@ -29,9 +29,9 @@ echo "Setup Spree Core and running RSpec..."
 cd ../core; set_gemfile; bundle update; bundle exec rspec spec
 
 # Spree Frontend
-echo "Setup Spree Frontend and running RSpec..."
-cd ../frontend; set_gemfile; bundle update; bundle exec rspec spec
-
-# Spree Sample
-echo "Setup Spree Sample and running RSpec..."
-cd ../sample; bundle install; bundle exec rake test_app; bundle exec rspec spec
+# echo "Setup Spree Frontend and running RSpec..."
+# cd ../frontend; set_gemfile; bundle update; bundle exec rspec spec
+# 
+# # Spree Sample
+# echo "Setup Spree Sample and running RSpec..."
+# cd ../sample; bundle install; bundle exec rake test_app; bundle exec rspec spec
